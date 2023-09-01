29dbd75d7c92bd85363346b9b168e4f1f9b69eb1
Riyad <Riyad9876@gmail.com>
2023-09-01 11:56:51 -0400

    models/Flight.js , model & schema

---
+++ models/Flight.js
@@ -0,0 +1,23 @@
+const mongoose = require('mongoose')
+
+
+const flightSchema = new mongoose.Schema({
+    airline: {
+        type: String,
+        required: true,
+        enum: ['American', 'Southwest', 'United']
+    },
+    flight: {
+        type: Number,
+        required: true,
+        max : 9999,
+        min : 10
+    },
+    departs : {
+        type : Date,
+    }
+})
+
+const Flight = mongoose.model('Flight', flightSchema)
+
+module.exports = Flight
--- server.js
+++ server.js
@@ -5,6 +5,8 @@ const express = require('express')
 const app = express()
 const PORT = 3000
 
+const Flight = require('./models/Flight')
+
 connectDB()
 
 
